d89f691c394a4bdad6a30ef0cc7acacf842dd2f1
Eric Gaston Haas <ericgaston1234@gmail.com>
2019-10-11 02:56:04 -0300

    Rutas reparadas
    rutas 100%

--- application/views/index.php
+++ application/views/index.php
@@ -7,20 +7,20 @@
 
     <link href="https://fonts.googleapis.com/css?family=Lobster&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" 
+    <link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
 
     <!-- this is for favicon from realfavicongenerator.net -->
-    <link rel="apple-touch-icon" sizes="180x180" href="./favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="./favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="./favicon/favicon-16x16.png">
-    <link rel="manifest" href="./favicon/site.webmanifest">
-    <link rel="mask-icon" href="./favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= base_url()?>assets/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= base_url()?>assets/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= base_url()?>assets/favicon/favicon-16x16.png">
+    <link rel="manifest" href="<?= base_url()?>assets/favicon/site.webmanifest">
+    <link rel="mask-icon" href="<?= base_url()?>assets/favicon/safari-pinned-tab.svg" color="#5bbad5">
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#ffffff">
 
-    <link type="text/css" rel="stylesheet" href="./master.css">
-    <link rel="stylesheet" href="./posts.css">
+    <link type="text/css" rel="stylesheet" href="<?= base_url()?>assets/css/master.css">
+    <link rel="stylesheet" href="<?= base_url()?>assets/css/posts.css">
 
     <title>LCdLV</title>
 </head>
@@ -32,8 +32,8 @@
             <a href="">Archivo</a>
         </nav>
         <form class="form-search" action="">
-            <input class="input-search" type="search" 
-                autofocus 
+            <input class="input-search" type="search"
+                autofocus
                 placeholder="Buscar...">
         </form>
 
@@ -45,7 +45,7 @@
                     <i class="fab fa-facebook"></i>
                 </a>
             </div>
-        
+
             <div id="icon-div2">
                 <a class="social-icons"
                     href="https://www.instagram.com/lacaradelavergha/"
@@ -53,7 +53,7 @@
                     <i class="fab fa-instagram"></i>
                 </a>
             </div>
-        
+
         </div>
     </header>
     <main id="main">
@@ -64,14 +64,14 @@
         <!-- all this is the template for dynamic loadin from backend and/or db -->
         <article id="article-post">
             <h2 class="h2-title">TITULO POST</h2>
-            
+
             <div class="div-time">
                 <time class="time-post">fecha y hora de post</time>
             </div>
 
             <div class="div-img">
-                <img class="img-post" 
-                    src="./img/mosquitos.jpg" 
+                <img class="img-post"
+                    src="<?=base_url()?>assets/img/mosquitos.jpg"
                     alt="">
             </div>
 
@@ -102,18 +102,18 @@
             </table>
 
             <form class="form-comments"action="">
-                <input class="input-comments" 
-                    type="text" 
+                <input class="input-comments"
+                    type="text"
                     placeholder="Escribe un comentario...">
             </form>
 
-        </article>        
+        </article>
     </main>
     <aside id="aside">
         aside
     </aside>
     <footer>
         footer
-    </footer>  
+    </footer>
 </body>
 </html>
