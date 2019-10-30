caf2519cc3cd8b788986f2ba162e58e9a128e741
Eric Gaston Haas <ericgaston1234@gmail.com>
2019-10-30 04:49:47 -0300

    Agrego acceso a BDD
    Acceso a BDD credenciales, modelo home y controlador home

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,8 +3,32 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 
-	public function index()
-	{
+	public function __construct() {
+			Parent::__construct();
+			$this->load->model("Home_model","home");
+	}
+
+	public function index(){
 		$this->load->view('index');
 	}
+
+	public function cargar_fotos(){
+		$resultado = $this->home->get_fotos();
+		$data = array();
+		//creo array asociativo con el resultado de la BDD
+		foreach($resultado->result() as $r){
+			$data[] = array(
+				$r->id,
+				$r->titulo,
+				$r->fecha,
+				$r->descripcion,
+				base64_encode($r->foto)
+			);
+		}
+		$output = array(
+			'data' => $data
+		);
+		echo json_encode($output);
+	}
 }
+?>
---
+++ application/models/Home_model.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home_model extends CI_Model{
+    var $table_fotos ="fotos";
+    var $table_coment ="comentarios";
+
+    public function __construct(){
+        parent::__construct();
+        $this->load->database();
+    }
+
+    public function get_fotos(){
+      //$resultado = $this->db->from($this->table_fotos);
+
+      $query = $this->db->get($this->table_fotos);
+      return $query;
+    }
+}
+?>
