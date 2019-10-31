c85bef2dc11a930b86e47ff56efe3ee0184ac850
NumaX <37051017+GabrielNumaX@users.noreply.github>
2019-10-31 15:59:43 -0300

    footer changed gateric added

--- application/views/archivo.php
+++ application/views/archivo.php
@@ -321,15 +321,32 @@
     </main>
 
     <footer id="footer">
-        <div class="footer-div">
-            <h4>NumaX &copy; 2019</h4>
-        </div>
-        <div class="footer-icons">
-        <a href="https://github.com/GabrielNumaX/"><i class="fab fa-github-square"></i></a>
-            <a href="mailto:g.numa10@hotmail.com"><i class="fas fa-envelope"></i></a>
-            <a href="https://www.linkedin.com/in/numax/"><i class="fab fa-linkedin"></i></a>
+        <div class="footer-div1">
+            <h4>Front End</h4>
+            <h4>NumaX &copy; 2020</h4>
 
+            <div class="footer-icons1">
+
+                <a href="https://github.com/GabrielNumaX/" target="_blank"><i class="fab fa-github-square"></i></a>
+                <a href="mailto:g.numa10@hotmail.com" target="_blank"><i class="fas fa-envelope"></i></a>
+                <a href="https://www.linkedin.com/in/numax/" target="_blank"><i class="fab fa-linkedin"></i></a>
+
+            </div>
         </div>
+        
+        <div class="footer-div2">
+            <h4>Back End</h4>
+            <h4>Eric &copy; 2020</h4>
+
+            <div class="footer-icons2">
+                    
+                <a href="https://github.com/Eric19906913/" target="_blank"><i class="fab fa-github-square"></i></a>
+                <a href="mailto:ericgaston1234@gmail.com" target="_blank"><i class="fas fa-envelope"></i></a>
+                <a href="https://www.linkedin.com/in/haasericgaston/" target="_blank"><i class="fab fa-linkedin"></i></a>
+
+            </div>
+        </div>
+        
     </footer>
 
 </body>
